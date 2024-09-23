e256932d0816a199447daed6092ffe1c935aaeb6
Anwar Ahmed <nileking2007@gmail.com>
2024-09-23 22:01:27 +0300

    fix: add documentation to bubble sort algorithm

--- algorithms/sort/bubbleSort.js
+++ algorithms/sort/bubbleSort.js
@@ -2,11 +2,11 @@ function sort(arr) {
     let swapped;
 
     do {
-        swapped = false;
+        swapped = false;  // set swapped to false at the start of each iteration
         for (let i = 0; i < arr.length - 1; i++) {
             if (arr[i] > arr[i + 1]) {
-                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-                swapped = true;
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]; // swap elements
+                swapped = true;  // set swapped to true if a swap was made
             }
             console.log(arr);
         }
