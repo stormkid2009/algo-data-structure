4428bfca31aa8bb4efc7ba59102bb0b546d0fb41
Anwar Ahmed <nileking2007@gmail.com>
2024-09-23 22:24:44 +0300

    fix: update quick sort documentation

--- algorithms/sort/quickSort.js
+++ algorithms/sort/quickSort.js
@@ -1,35 +1,31 @@
 //time complexity O(nlogn)
 // we use divide and conquer approach
-//in this algorithm we divide the array into two parts by selecting the last element as the pivot
-//then we compare each element with the pivot
-//if the element is less than the pivot we push it to the left array
-//if the element is greater than the pivot we push it to the right array
-//then we recursively call the quick sort function on the left and right arrays
-//then we return the sorted array
-//we do this until the array has only one element
 // it uses a lot of memory rescources so it is not recommended for large arrays
-// we can optimize it in version 2
+// we can optimize it in reucrisve version
 
+/**
+ * Sorts an array using the quicksort algorithm.
+ *
+ * @param {Array} arr - the array to be sorted
+ * @return {Array} the sorted array
+ */
 function quickSort(arr) {
     if(arr.length < 2) {
         return arr;
     }
-    let pivot = arr[arr.length - 1];
+    let pivot = arr[arr.length - 1]; // last element is pivot
     let left = [];
     let right = [];
     for(let i = 0; i < arr.length - 1; i++) {
         if(arr[i] < pivot) {
-            left.push(arr[i]);
+            left.push(arr[i]); // elements less than pivot go to left
         } else {
-            right.push(arr[i]);
+            right.push(arr[i]); // elements greater than pivot go to right
         }
     }
-    //console.log(left, pivot, right)       to see how the array is divided and how many arrays we have
     return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 
 let list = [22, 11, 5, 33, 17,101,202,44,37,555,22,11,5,33,17];
 console.log(quickSort(list))
-//let newList = [22,11,5,33,17]
-//quickSort(list)
