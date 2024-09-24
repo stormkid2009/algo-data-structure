4ef0f9c02900b6927babbb84b084579a1d8b9f06
Anwar Ahmed <nileking2007@gmail.com>
2024-09-24 10:24:49 +0300

    fix: update linear search algorithm documentaion

--- algorithms/search/linearSearch.js
+++ algorithms/search/linearSearch.js
@@ -1,10 +1,16 @@
 // implementaion of linear search algoritm
 // Time complexity O(n)
+
+/**
+ * Performs a linear search on a given array to find the index of a target value.
+ *
+ * @param {array} arr - The array to be searched.
+ * @param {any} target - The target value to search for.
+ * @return {number} The index of the target value if found, -1 otherwise.
+ */
 function linearSearch(arr, target) {
     for (let i = 0; i < arr.length; i++) {
-        //console.log("i before if condition: ", i);
         if (arr[i] === target) {
-            //console.log("i after if condition: ", i);
             return i;
         }
     }
