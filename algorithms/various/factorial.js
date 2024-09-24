fb646577fed86c85ce5b32f477ffc200c9b8d4a8
Anwar Ahmed <nileking2007@gmail.com>
2024-09-24 10:39:09 +0300

    fix: update docs of get factorial num function

--- algorithms/various/factorial.js
+++ algorithms/various/factorial.js
@@ -1,6 +1,12 @@
 //A factorial of a non-negative integer ( n ) is the product of all positive integers less than or equal to ( n ).
 //For example: 5! = 5 * 4 * 3 * 2 * 1 = 120
 
+/**
+ * Calculates the factorial of a given non-negative integer.
+ *
+ * @param {number} num - the non-negative integer to calculate the factorial for
+ * @return {number} the factorial of the given number, or -1 if the input is negative
+ */
 function factorial(num) {   
     if (num < 0) return -1;
     if (num === 0) return 1;
