4e942f1680f7c5df0f9b9fd7c2edae89967b25b4
Anwar Ahmed <nileking2007@gmail.com>
2024-09-24 10:37:44 +0300

    fix: update recursive version of get  factorial num function

--- algorithms/various/factoRecursive.js
+++ algorithms/various/factoRecursive.js
@@ -1,6 +1,12 @@
 // the factorial of a non-negative integer ( n ) is the product of all positive integers less than or equal to ( n ).
 // For example: 5! = 5 * 4 * 3 * 2 * 1 = 120
 
+/**
+ * Calculates the factorial of a non-negative integer using recursion.
+ *
+ * @param {number} n - the non-negative integer to calculate the factorial of
+ * @return {number} the factorial of the given integer
+ */
 function factoRecursive(n){
 
     //base case
