efa32bbec4b7a2524edc7c28c440d68b3bdc9a85
Anwar Ahmed <nileking2007@gmail.com>
2024-09-24 11:03:34 +0300

    fix: update docs of fibonacci algorithm

--- algorithms/various/fibonacci.js
+++ algorithms/various/fibonacci.js
@@ -1,6 +1,12 @@
 //The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones.
 //It typically starts with 0 and 1. So, the sequence goes: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, and so on
 
+/**
+ * Generates the Fibonacci sequence up to the nth number.
+ *
+ * @param {number} num - the number of elements in the Fibonacci sequence to generate
+ * @return {array|number} the generated Fibonacci sequence as an array, or 0 if num is 0, or an empty array if num is negative
+ */
 let fibonacci = (num) => {
   if (num < 0) return [];
   if (num === 0) return 0;
