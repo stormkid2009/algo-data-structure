f317b22cd7142d58f6ee827ec2283ab29211d2ef
Anwar Ahmed <nileking2007@gmail.com>
2024-09-24 11:10:37 +0300

    fix: update docs of recursion implementation

--- algorithms/various/recursiveFunc.js
+++ algorithms/various/recursiveFunc.js
@@ -1,4 +1,10 @@
 
+/**
+ * A recursive function that prints the numbers from the input number down to 1.
+ *
+ * @param {number} n - the starting number for the recursive countdown
+ * @return {undefined} this function does not return a value
+ */
 function recursive(n){
     console.log(`rescursive start at number : ${n} `);
     if(n > 1){
