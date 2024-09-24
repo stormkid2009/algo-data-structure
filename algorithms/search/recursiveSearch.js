66a4c3b8994fa5a97d4812035f72d76ca7130a96
Anwar Ahmed <nileking2007@gmail.com>
2024-09-24 10:33:04 +0300

    fix: upate recursive version of linear search docs

--- algorithms/search/recursiveSearch.js
+++ algorithms/search/recursiveSearch.js
@@ -1,3 +1,11 @@
+/**
+ * Performs a recursive linear search on an array to find the index of a target value.
+ *
+ * @param {array} arr - The array to be searched.
+ * @param {any} target - The target value to search for.
+ * @param {number} [index=0] - The starting index of the search.
+ * @return {number} The index of the target value if found, -1 otherwise.
+ */
 function recursiveSearch(arr, target, index = 0) {
     if (index >= arr.length) {
         return -1;
