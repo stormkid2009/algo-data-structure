2a67768bb5c2dce12f7e3d9568ccd54f86f0bd7d
Anwar <anwar@192.168.1.11>
2024-09-27 18:17:30 +0300

    feat: add climb Stair Case algorithm

---
+++ algorithms/various/climbStairCase.js
@@ -0,0 +1,42 @@
+// Time complexity O(2^n)
+
+/**
+ * Calculates the number of ways to climb to the top of a staircase with n steps, 
+ * where you can either climb 1 or 2 steps at a time.
+ *
+ * @param {number} n - The number of steps in the staircase.
+ * @return {number} The number of ways to climb to the top.
+ */
+function climbStairCase(n) {
+    if (n <= 0) return 0;
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+    return climbStairCase(n - 1) + climbStairCase(n - 2);
+}
+
+
+// Time complexity O(n)
+
+/**
+ * Calculates the number of ways to climb to the top of a staircase with n steps, 
+ * where you can either climb 1 or 2 steps at a time.
+ *
+ * @param {number} n - The number of steps in the staircase.
+ * @return {number} The number of ways to climb to the top.
+ */
+function climbStairCaseV2(n) {
+    
+        let noOfWays = [1,2];
+        for(let i = 2 ; i <= n; i++){
+            noOfWays[i] = noOfWays[i-1] + noOfWays[i-2]
+        }
+        
+        return noOfWays[n-1]
+    }
+
+
+console.log(climbStairCase(1));
+console.log(climbStairCase(2));
+console.log(climbStairCase(3));
+console.log(climbStairCase(4));
+console.log(climbStairCase(5));
