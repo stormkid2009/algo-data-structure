107021d8ae713da3d07365e7f45aaac44ec3581f
Anwar Ahmed <nileking2007@gmail.com>
2024-10-12 15:21:58 +0300

    feat: update testing file to test doubly methods

--- data-structure/linked-list/testing.js
+++ data-structure/linked-list/testing.js
@@ -1,3 +1,5 @@
+// TESTING LINKED LIST
+/*
 const LinkedList = require('./singly');
 const list = new LinkedList();
 list.append(10).append(20).append(30).prepend(5);
@@ -13,6 +15,27 @@ console.log(list.removeByValue(20)); // 20
 console.log(list.isEmpty()); // false
 console.log(list.getSize()); // 1
 console.log(list.removeByValue(10)); // 10
+console.log(list.isEmpty()); // true
+console.log(list.getSize()); // 0
+console.log(list.print()); // null
+
+*/
+
+const LinkedList = require('./doubly');
+const list = new LinkedList();
+list.append(10).append(20).append(30).prepend(5);
+console.log(list.print()); // 5 -> 10 -> 20 -> 30 -> null
+console.log(list.isEmpty()); // false
+console.log(list.getSize()); // 4
+console.log(list.removeByValue(30)); // 30
+console.log(list.removeByValue(5)); // 5
+console.log(list.print()); // 10 -> 20 -> null
+console.log(list.getSize()); // 2
+console.log(list.isEmpty()); // false
+console.log(list.remove(1)); // 20
+console.log(list.isEmpty()); // false
+console.log(list.getSize()); // 1
+console.log(list.remove(0)); // 10
 console.log(list.isEmpty()); // true
 console.log(list.getSize()); // 0
 console.log(list.print()); // null
