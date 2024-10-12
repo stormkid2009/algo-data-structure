3e2848138f775086744ee1b59270ae1499a2fcad
Anwar Ahmed <nileking2007@gmail.com>
2024-10-12 21:01:28 +0300

    fix: update removeByValue method to delete all occurences

--- data-structure/linked-list/doubly.js
+++ data-structure/linked-list/doubly.js
@@ -1,155 +1,161 @@
 class Node {
-    constructor(value){
-        this.value = value
-        this.next = null
-        this.prev = null
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
 }
 
 class LinkedList {
-    constructor(){
-        this.head = null
-        this.tail = null
-        this.size = 0
-    }
-    isEmpty(){
-        return this.size === 0
-    }
-    
-    getSize(){
-        return this.size
-    }
-    
-    append(value){
-        const node = new Node(value)
-        if(this.isEmpty()){
-            this.head = this.tail = node
-        }else{
-            node.prev = this.tail
-            this.tail.next = node
-            this.tail = node; // Moved this line inside else block for clarity
-        }
-        this.size++
-        return this
-    }
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+  isEmpty() {
+    return this.size === 0;
+  }
 
-    prepend(value){
-        const node = new Node(value)
-        if(this.isEmpty()){
-            this.head = this.tail = node
-        }else{
-            node.next = this.head
-            this.head.prev = node
-            this.head = node
-        }
-        this.size++
-        return this
-    }
+  getSize() {
+    return this.size;
+  }
 
-    insert(value, index){
-        if(index < 0 || index > this.size){
-            throw new Error("Invalid index")
-        }
-        if(index === 0){
-            return this.prepend(value)
-        }
-        if(index === this.size){
-            return this.append(value)
-        }
-        const node = new Node(value)
-        let current = this.head
-        for(let i = 0; i < index-1; i++){
-            current = current.next
-        }
-        node.next = current.next // link the new node to the next node
-        node.prev = current     // link the new node to the previous node
-        current.next.prev = node // link the next node prev ptr  to the new node
-        current.next = node      // link the previous  node next ptr to the new node
-        this.size++
-        return this
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = this.tail = node;
+    } else {
+      node.prev = this.tail;
+      this.tail.next = node;
+      this.tail = node; // Moved this line inside else block for clarity
     }
+    this.size++;
+    return this;
+  }
 
-    remove(index) {
-        if (index < 0 || index >= this.size) {
-            throw new Error("Invalid index");
-        }
-        let removedNode;
-        if (index === 0) {
-            removedNode = this.head;
-            this.head = this.head.next;
-            // if we still have a head, set its prev to null
-            if (this.head) {
-                this.head.prev = null;
-            } else {
-                this.tail = null; // List is now empty
-            }
-        } else if (index === this.size - 1) {
-            removedNode = this.tail;
-            this.tail = this.tail.prev;
-            // if we still have a tail, set its next to null
-            if (this.tail) {
-                this.tail.next = null;
-            } else {
-                this.head = null; // List is now empty
-            }
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head.prev = node;
+      this.head = node;
+    }
+    this.size++;
+    return this;
+  }
+
+  insert(value, index) {
+    if (index < 0 || index > this.size) {
+      throw new Error("Invalid index");
+    }
+    if (index === 0) {
+      return this.prepend(value);
+    }
+    if (index === this.size) {
+      return this.append(value);
+    }
+    const node = new Node(value);
+    let current = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      current = current.next;
+    }
+    node.next = current.next; // link the new node to the next node
+    node.prev = current; // link the new node to the previous node
+    current.next.prev = node; // link the next node prev ptr  to the new node
+    current.next = node; // link the previous  node next ptr to the new node
+    this.size++;
+    return this;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.size) {
+      throw new Error("Invalid index");
+    }
+    let removedNode;
+    if (index === 0) {
+      removedNode = this.head;
+      this.head = this.head.next;
+      // if we still have a head, set its prev to null
+      if (this.head) {
+        this.head.prev = null;
+      } else {
+        this.tail = null; // List is now empty
+      }
+    } else if (index === this.size - 1) {
+      removedNode = this.tail;
+      this.tail = this.tail.prev;
+      // if we still have a tail, set its next to null
+      if (this.tail) {
+        this.tail.next = null;
+      } else {
+        this.head = null; // List is now empty
+      }
+    } else {
+      let current = this.head;
+      for (let i = 0; i < index; i++) {
+        current = current.next;
+      }
+      removedNode = current;
+      current.prev.next = current.next;
+      current.next.prev = current.prev;
+    }
+    this.size--;
+    return removedNode.value;
+  }
+
+  removeByValue(value) {
+    let current = this.head;
+    let found = false; // Flag to track if the value is found and removed
+    while (current) {
+      if (current.value === value) {
+        found = true; // We found at least one node with the value
+        if (current === this.head) {
+          this.head = current.next;
+          // if we still have a head, set its prev to null
+          if (this.head) {
+            this.head.prev = null;
+          } else {
+            this.tail = null; // List is now empty
+          }
+        } else if (current === this.tail) {
+          this.tail = current.prev;
+          // if we still have a tail, set its next to null
+          if (this.tail) {
+            this.tail.next = null;
+          } else {
+            this.head = null; // List is now empty
+          }
+          // if current is somewhere in the middle of the list
         } else {
-            let current = this.head;
-            for (let i = 0; i < index; i++) {
-                current = current.next;
-            }
-            removedNode = current;
-            current.prev.next = current.next;
-            current.next.prev = current.prev;
+          current.prev.next = current.next;
+          current.next.prev = current.prev;
         }
         this.size--;
-        return removedNode.value;
+        // Move to the next node (which is now current.next)
+        current = current.next;
+      } else {
+        // Only move to the next node if no removal occurred
+        current = current.next;
+      }
     }
-    
+    // Return based on whether we found any node with the given value
+    return found ? value : null;
+  }
 
-    removeByValue(value) {
-        let current = this.head;
-        while (current) {
-            if (current.value === value) {
-                if (current === this.head) {
-                    this.head = current.next;
-                    // if we still have a head, set its prev to null
-                    if (this.head) {
-                        this.head.prev = null;
-                    } else {
-                        this.tail = null; // List is now empty
-                    }
-                } else if (current === this.tail) {
-                    this.tail = current.prev;
-                    // if we still have a tail, set its next to null
-                    if (this.tail) {
-                        this.tail.next = null;
-                    } else {
-                        this.head = null; // List is now empty
-                    }
-                } else {
-                    current.prev.next = current.next;
-                    current.next.prev = current.prev;
-                }
-                this.size--;
-                return value; // Returns the first occurrence
-            }
-            current = current.next;
-        }
-        return null; // Return null if no match is found
+  print() {
+    let output = [];
+    let current = this.head;
+    while (current) {
+      output.push(current.value);
+      current = current.next;
     }
-    
-    print(){
-        let output = []
-        let current = this.head
-        while(current){
-            output.push(current.value)
-            current = current.next
-        }
-        return "null <- -> " + output.join(" <- -> ") + " <- -> null"
-    }
+    return "null <- -> " + output.join(" <- -> ") + " <- -> null";
+  }
 }
 
-module.exports = LinkedList
+module.exports = LinkedList;
 /*
 const list = new LinkedList()
 console.log(list.isEmpty())
--- data-structure/linked-list/testing.js
+++ data-structure/linked-list/testing.js
@@ -23,19 +23,9 @@ console.log(list.print()); // null
 
 const LinkedList = require('./doubly');
 const list = new LinkedList();
-list.append(10).append(20).append(30).prepend(5);
-console.log(list.print()); // 5 -> 10 -> 20 -> 30 -> null
-console.log(list.isEmpty()); // false
-console.log(list.getSize()); // 4
+list.append(10).append(20).append(30).prepend(5).append(40).prepend(30);
+console.log(list.print()); // null <- 30 -><- 5 -> <- 10 -> <- 20 -> <- 30 -> <- 40 -> null
 console.log(list.removeByValue(30)); // 30
-console.log(list.removeByValue(5)); // 5
-console.log(list.print()); // 10 -> 20 -> null
-console.log(list.getSize()); // 2
-console.log(list.isEmpty()); // false
-console.log(list.remove(1)); // 20
-console.log(list.isEmpty()); // false
-console.log(list.getSize()); // 1
-console.log(list.remove(0)); // 10
-console.log(list.isEmpty()); // true
-console.log(list.getSize()); // 0
-console.log(list.print()); // null
+console.log(list.print()); // null <- 5 -> <- 10 -> <- 20 -> <- 40 -> null
+console.log(list.removeByValue(100)); // null (since 100 is not in the list)
+console.log(list.print()); // null <- 5 -> <- 10 -> <- 20 -> <- 40 -> null
