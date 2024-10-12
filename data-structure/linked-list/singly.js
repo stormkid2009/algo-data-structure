634fa6b2875b912180510bb0a683fff64ae98862
Anwar Ahmed <nileking2007@gmail.com>
2024-10-12 14:21:29 +0300

    fix: refactor singly linked list methods to keep consistency

--- data-structure/linked-list/singly.js
+++ data-structure/linked-list/singly.js
@@ -1,4 +1,5 @@
-// return this key word at the end of methodes allow methods chaining
+// return this key word at the end of methodes allow methods chaining like append prepend insert
+// but we prefer to return the value from some methods like remove and removeByValue
 
 
 class Node {
@@ -12,15 +13,15 @@ class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
-    this.length = 0;
+    this.size = 0;
   }
 
   isEmpty() {
-    return this.length === 0;
+    return this.size === 0;
   }
 
-  getLength() {
-    return this.length;
+  getSize() {
+    return this.size;
   }
 
   prepend(value) {
@@ -32,7 +33,7 @@ class LinkedList {
       node.next = this.head;
       this.head = node;
     }
-    this.length++;
+    this.size++;
     return this; // Enable chaining
   }
 
@@ -45,12 +46,12 @@ class LinkedList {
       this.tail.next = node;
       this.tail = node;
     }
-    this.length++;
+    this.size++;
     return this; // Enable chaining
   }
 
   insert(value, index) {
-    if (index < 0 || index > this.length) {
+    if (index < 0 || index > this.size) {
       throw new Error(`Invalid index ${index}`);
     }
 
@@ -59,7 +60,7 @@ class LinkedList {
       return this.prepend(value);
     }
 
-    if (index === this.length) {
+    if (index === this.size) {
       return this.append(value);
     }
 
@@ -71,18 +72,19 @@ class LinkedList {
 
     node.next = prev.next;
     prev.next = node;
-    this.length++;
+    this.size++;
     return this; // Enable chaining
   }
 
   remove(index) {
-    if (index < 0 || index >= this.length) {
+    let removedNode;
+    if (index < 0 || index >= this.size) {
       throw new Error(`Invalid index ${index}`);
     };
     if (index === 0) {
-      const removedNode = this.head;
+       removedNode = this.head;
       this.head = this.head.next;
-      if (this.length === 1) {
+      if (this.size === 1) {
         this.tail = null;
       }
     } else {
@@ -90,7 +92,7 @@ class LinkedList {
       for (let i = 0; i < index - 1; i++) {
         prev = prev.next;
       }
-      const removedNode = prev.next;
+       removedNode = prev.next;
       prev.next = prev.next.next;
       if (!prev.next) {
         this.tail = prev;
@@ -98,23 +100,24 @@ class LinkedList {
       
     }
 
-    this.length--;
-    return this; // Enable chaining
+    this.size--;
+    return removedNode.value; // Return the removed node
   }
 
   removeByValue(value) {
     if (this.isEmpty()) {
       throw new Error("List is empty"); // control user beahvior to protect chaining methons from errors
     }
-
+    let removedNode;
     let current = this.head;
     let prev = null; // Initially, prev is null because we haven't moved past the head.
     while (current) {
       if (current.value === value) {
+        removedNode = current;
         // If the node to remove is the head
         if (prev === null) {
           this.head = current.next; //if it is the only node we set head to null
-          if (this.length === 1) { // If it's the only node
+          if (this.size === 1) { // If it's the only node
             this.tail = null;
           }
         } else {
@@ -124,8 +127,8 @@ class LinkedList {
         if (!current.next) {
           this.tail = prev;
         }
-        this.length--;
-        return this;
+        this.size--;
+        return removedNode.value;
       }
       prev = current;
       current = current.next;
