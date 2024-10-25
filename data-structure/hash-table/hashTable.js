9ef56fa4a941a91fb8266b8666ede4c0d9389cd4
Anwar Ahmed <nileking2007@gmail.com>
2024-10-25 20:51:31 +0300

    feat: add hash table data structure

--- data-structure/hash-table/hashTable.js
+++ data-structure/hash-table/hashTable.js
@@ -1,18 +1,46 @@
 
-
-
-// hash function
-function hashStringToInt(s, tableSize) {
-    let hash = 5381;  //special constant for the hash algorithm
-    for (let i = 0; i < s.length; i++) {
-        hash = (hash * 33) ^ s.charCodeAt(i);
+class HashTable {
+    constructor(size = 53) {
+        this.keyMap = new Array(size);
+        this.size = size;
     }
-    // Ensure hash is a positive integer and fit within the table size
-    return Math.abs(hash) % tableSize;
+
+    _hash(key) {
+        let hash = 5381;  //special constant for the hash algorithm
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash * 33) ^ key.charCodeAt(i);
+        }
+        // Ensure hash is a positive integer and fit within the table size
+        return Math.abs(hash) % this.size;
+    }
+
+    set(key, value) {
+        const index = this._hash(key);
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value]);
+    }
+
+    get(key) {
+        const index = this._hash(key);
+        if (this.keyMap[index]) {
+            for (let i = 0; i < this.keyMap[index].length; i++) {
+                if (this.keyMap[index][i][0] === key) {
+                    return this.keyMap[index][i][1];
+                }
+            }
+        }
+        return undefined;
+    }
 }
 
-// Example usage
-const key = "exampleKey";
-const tableSize = 100;
-const hashValue = hashStringToInt(key, tableSize);
-console.log(`Hash value for "${key}" is: `, hashValue);
+let table = new HashTable();
+
+table.set("hello", "world");
+table.set("foo", "bar");
+console.log(table.get("hello")); // Output: world
+console.log(table.get("foo")); // Output: bar
+console.log(table.get("eg")); // Output: undefined
+
+
