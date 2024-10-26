a7606618ebdc44a9424cf61d960f95eb6715312b
Anwar Ahmed <nileking2007@gmail.com>
2024-10-26 15:24:24 +0300

    feat: add _resize method and refactor code

--- data-structure/hash-table/hashTable.js
+++ data-structure/hash-table/hashTable.js
@@ -1,8 +1,11 @@
+import checkPrime from "../../algorithms/various/primeNum.js";
 
 class HashTable {
     constructor(size = 53) {
         this.keyMap = new Array(size);
         this.size = size;
+        this.itemCount = 0; // keep track of the number of sorted items
+        this.loadFactorThreshold = 0.7;
     }
 
     _hash(key) {
@@ -11,7 +14,30 @@ class HashTable {
             hash = (hash * 33) ^ key.charCodeAt(i);
         }
         // Ensure hash is a positive integer and fit within the table size
-        return Math.abs(hash) % this.size;
+        return (hash >>> 0) % this.size;
+    }
+
+    _resize() {
+        const newSize = this._findNextPrime(this.size * 2);
+        const oldKeyMap = this.keyMap;
+        this.keyMap = new Array(newSize);
+        this.size = newSize;
+        this.itemCount = 0;
+
+        for (let bucket of oldKeyMap) {
+            if (bucket) {
+                for (let[key, value] of bucket) {
+                    this.set(key, value);
+                }
+            }
+        }
+    }
+
+    _findNextPrime(num) {
+        while (!checkPrime(num)) {
+            num++;
+        }
+        return num;
     }
 
     set(key, value) {
@@ -19,7 +45,18 @@ class HashTable {
         if (!this.keyMap[index]) {
             this.keyMap[index] = [];
         }
+        for(let i=0; i<this.keyMap[index].length; i++) {
+            if(this.keyMap[index][i][0] === key) {
+                this.keyMap[index][i][1] = value;
+                return `existing key ${key} has been updated`;
+            }
+        }
         this.keyMap[index].push([key, value]);
+        this.itemCount++;
+
+        if (this.itemCount / this.size > this.loadFactorThreshold) {
+            this._resize();
+        }
     }
 
     get(key) {
@@ -33,14 +70,24 @@ class HashTable {
         }
         return undefined;
     }
+
+    display() {
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                console.log(i, this.keyMap[i]);
+            }
+        }
+    }
 }
 
 let table = new HashTable();
 
 table.set("hello", "world");
 table.set("foo", "bar");
-console.log(table.get("hello")); // Output: world
-console.log(table.get("foo")); // Output: bar
-console.log(table.get("eg")); // Output: undefined
+table.set("good morning", "bonjour");
+table.set("good afternoon", "bonsoir");
+table.set("good night", "bonne nuit");
+console.log(table.set("hello", "salut"));
+// table.display();
 
-
+console.log(table.get("hello"));
