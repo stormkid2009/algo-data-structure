35d8522410a238cc38396e7872046049c3f5c95d
Anwar Ahmed <nileking2007@gmail.com>
2024-11-04 22:46:05 +0200

    feat: add inOrder and postOrder methods

--- data-structure/trees/binarySearchTree.js
+++ data-structure/trees/binarySearchTree.js
@@ -1,92 +1,103 @@
-
-
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-
 class BinarySearchTree {
-    constructor() {
-        this.root = null;
-    }
+  constructor() {
+    this.root = null;
+  }
 
-    isEmpty() {
-        return this.root === null;
+  isEmpty() {
+    return this.root === null;
+  }
+  // binary search tree rules
+  // 1. left node value < parent node value
+  // 2. right node value > parent node value
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.root = newNode;
+      return this;
+    } else {
+      this.insertNode(this.root, newNode);
     }
-    // binary search tree rules
-    // 1. left node value < parent node value
-    // 2. right node value > parent node value
-    insert(value) {
-        const newNode = new Node(value);
-        if (this.isEmpty()) {
-            this.root = newNode;
-            return this;
-        } else {
-            this.insertNode(this.root, newNode);
-        }
-    }
+  }
 
-    insertNode(node, newNode) {
-        if (newNode.value < node.value) {
-            if (node.left === null) {
-                node.left = newNode;
-            } else {
-                this.insertNode(node.left, newNode);
-            }
-        } else {
-            if (node.right === null) {
-                node.right = newNode;
-            } else {
-                this.insertNode(node.right, newNode);
-            }
-        }
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
     }
+  }
 
-    search(node, value) {
-        if (!node) {
-            return false;
-        }
-        if (node.value === value) {
-            return true;
-        } else if (value < node.value) {
-            return this.search(node.left, value);
-        } else {
-            return this.search(node.right, value);
-        }
+  search(node, value) {
+    if (!node) {
+      return false;
     }
+    if (node.value === value) {
+      return true;
+    } else if (value < node.value) {
+      return this.search(node.left, value);
+    } else {
+      return this.search(node.right, value);
+    }
+  }
 
-    preOrder(node) {
-        if (node) {
-            console.log(node.value);
-            this.preOrder(node.left);
-            this.preOrder(node.right);
-        }
+  preOrder(node) {
+    if (node) {
+      console.log(node.value);
+      this.preOrder(node.left);
+      this.preOrder(node.right);
     }
-    
-    preOrderIterative(node) {
-        if(!node) {
-            return;
+  }
+
+  preOrderIterative(node) {
+    if (!node) {
+      return;
     }
     const stack = [];
     stack.push(node);
-    while(stack.length > 0) {
-        const current = stack.pop();
-        console.log(current.value);
-        if(current.right) {
-            stack.push(current.right);
-        }
-        if(current.left) {
-            stack.push(current.left);
-        }
+    while (stack.length > 0) {
+      const current = stack.pop();
+      console.log(current.value);
+      if (current.right) {
+        stack.push(current.right);
+      }
+      if (current.left) {
+        stack.push(current.left);
+      }
     }
-}
-        
-}
+  }
 
+  inOrder(node) {
+    if (node) {
+      this.inOrder(node.left);
+      console.log(node.value);
+      this.inOrder(node.right);
+    }
+  }
+
+  postOrder(node) {
+    if (node) {
+      this.postOrder(node.left);
+      this.postOrder(node.right);
+      console.log(node.value);
+    }
+  }
+}
 
 const bst = new BinarySearchTree();
 bst.insert(50);
@@ -103,4 +114,6 @@ bst.insert(80);
 // console.log(bst.search(bst.root, 10)); // expected false
 // console.log(bst.search(bst.root, 10)); // expected false
 // console.log(bst.search(bst.root, 10)); // expected false
-console.log(bst.preOrderIterative(bst.root));
+// console.log(bst.preOrderIterative(bst.root));
+console.log(bst.postOrder(bst.root));
+// console.log(bst.inOrder(bst.root));
