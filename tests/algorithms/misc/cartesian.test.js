e8e9f62a71f6b55732f9f960b84082faf755f151
Anwar Ahmed <nileking2007@gmail.com>
2024-11-21 21:51:12 +0200

    fix : refactor test for cartesian algorithm

--- tests/algorithms/misc/cartesian.test.js
+++ tests/algorithms/misc/cartesian.test.js
@@ -1,17 +1,41 @@
 
 import { cartesian } from "../../../src/algorithms/misc/cartesian";
 
-describe("cartesian", () => {   
-    test("returns the Cartesian product of two arrays", () => {
+
+describe("cartesian", () => {
+    it("returns the cartesian product of two arrays", () => {
         const arrA = [1, 2];
-        const arrB = [3, 4];
-        const expected = [[1, 3], [1, 4], [2, 3], [2, 4]];
-        expect(cartesian(arrA, arrB)).toEqual(expected);
+        const arrB = ['a', 'b'];
+        const result = cartesian(arrA, arrB);
+        expect(result).toEqual([
+            [1, 'a'],
+            [1, 'b'],
+            [2, 'a'],
+            [2, 'b']
+        ]);
     });
-    test("returns an empty array if one of the arrays is empty", () => {
-        const arrA = [];
-        const arrB = [1, 2];
-        const expected = [];
-        expect(cartesian(arrA, arrB)).toEqual(expected);
-    })
+
+    it("handles one or both arrays being empty", () => {
+        expect(cartesian([], [1, 2, 3])).toEqual([]); // Empty first array
+        expect(cartesian([1, 2, 3], [])).toEqual([]); // Empty second array
+        expect(cartesian([], [])).toEqual([]);        // Both arrays empty
+    });
+
+    it("handles arrays with one element each", () => {
+        const arrA = [5];
+        const arrB = ['x'];
+        const result = cartesian(arrA, arrB);
+        expect(result).toEqual([[5, 'x']]);
+    });
+
+    it("works for arrays of different lengths", () => {
+        const arrA = [1];
+        const arrB = [10, 20, 30];
+        const result = cartesian(arrA, arrB);
+        expect(result).toEqual([
+            [1, 10],
+            [1, 20],
+            [1, 30]
+        ]);
+    });
 });
